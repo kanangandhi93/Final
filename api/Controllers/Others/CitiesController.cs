30e435c14fab472158ffc65398e12c3d904a682c
kanangandhi93 <kanangandhi93@gmail.com>
2018-05-17 22:12:12 +0530

    Country, State City Completed.

---
+++ api/Controllers/Others/CitiesController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using api.Models;
+
+namespace api.Controllers.Others
+{
+    public class clsCity
+    {
+        public long ID { get; set; }
+        public long StateID { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string StateName { get; set; }
+    }
+    public class CitiesController : ApiController
+    {
+        private InventoryControlEntities db = new InventoryControlEntities();
+
+        // GET: api/Cities
+        public IEnumerable<clsCity> GetCities()
+        {
+            var rr= db.Cities;
+            List<clsCity> lstrep = new List<clsCity>();
+
+            foreach (var item in rr)
+            {
+                clsCity rep = new clsCity()
+                {
+                    Code = item.Code,
+                    ID = item.ID,
+                    StateID = item.StateID,
+                    Name = item.Name
+                };
+                if (item.StateID != null)
+                {
+                    rep.StateName = db.States.Where(x => x.ID == item.StateID).SingleOrDefault().Code;
+
+                }
+                lstrep.Add(rep);
+            }
+            return lstrep;
+        }
+
+        // GET: api/Cities/5
+        [ResponseType(typeof(City))]
+        public IHttpActionResult GetCity(long id)
+        {
+            City city = db.Cities.Find(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city);
+        }
+
+        // PUT: api/Cities/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCity(long id, City city)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != city.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(city).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Cities
+        [ResponseType(typeof(City))]
+        public IHttpActionResult PostCity(City city)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Cities.Add(city);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = city.ID }, city);
+        }
+
+        // DELETE: api/Cities/5
+        [ResponseType(typeof(City))]
+        public IHttpActionResult DeleteCity(long id)
+        {
+            City city = db.Cities.Find(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            db.Cities.Remove(city);
+            db.SaveChanges();
+
+            return Ok(city);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CityExists(long id)
+        {
+            return db.Cities.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ api/Controllers/Others/StatesController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using api.Models;
+
+namespace api.Controllers.Others
+{
+    public class clsState
+    {
+        public long ID { get; set; }
+        public long CountryID { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string CountryCode { get; set; }
+
+    }
+    public class StatesController : ApiController
+    {
+        private InventoryControlEntities db = new InventoryControlEntities();
+
+        // GET: api/States
+        public IEnumerable<clsState> GetStates()
+        {
+            var rr = db.States;
+
+            List<clsState> lstrep = new List<clsState>();
+
+            foreach (var item in rr)
+            {
+                clsState rep = new clsState()
+                {
+                   Code=item.Code,
+                   ID=item.ID,
+                   CountryID=item.CountryID,
+                   Name=item.Name
+                };
+                if (item.CountryID != null)
+                {
+                    rep.CountryCode = db.Countries.Where(x => x.ID == item.CountryID).SingleOrDefault().Code;
+
+                }
+                lstrep.Add(rep);
+            }
+            return lstrep;
+        }
+
+        // GET: api/States/5
+        [ResponseType(typeof(State))]
+        public IHttpActionResult GetState(long id)
+        {
+            State state = db.States.Find(id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(state);
+        }
+
+        // PUT: api/States/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutState(long id, State state)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != state.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(state).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/States
+        [ResponseType(typeof(State))]
+        public IHttpActionResult PostState(State state)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.States.Add(state);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = state.ID }, state);
+        }
+
+        // DELETE: api/States/5
+        [ResponseType(typeof(State))]
+        public IHttpActionResult DeleteState(long id)
+        {
+            State state = db.States.Find(id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            db.States.Remove(state);
+            db.SaveChanges();
+
+            return Ok(state);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool StateExists(long id)
+        {
+            return db.States.Count(e => e.ID == id) > 0;
+        }
+    }
+}

5fada679d0fa3eb3692e3115beaa274fecf211db
kanangandhi93 <kanangandhi93@gmail.com>
2018-05-17 22:14:44 +0530

    Menu Created.

---
+++ api/Controllers/apiMenuDir/MenusController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using api.Models;
+
+namespace api.Controllers.apiMenuDir
+{
+    public class MenusController : ApiController
+    {
+        private InventoryControlEntities db = new InventoryControlEntities();
+
+        // GET: api/Menus
+        public IQueryable<Menu> GetMenus()
+        {
+            return db.Menus;
+        }
+
+        // GET: api/Menus/5
+        [ResponseType(typeof(Menu))]
+        public IHttpActionResult GetMenu(long id)
+        {
+            Menu menu = db.Menus.Find(id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(menu);
+        }
+
+        // PUT: api/Menus/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutMenu(long id, Menu menu)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != menu.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(menu).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MenuExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Menus
+        [ResponseType(typeof(Menu))]
+        public IHttpActionResult PostMenu(Menu menu)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Menus.Add(menu);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (MenuExists(menu.ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = menu.ID }, menu);
+        }
+
+        // DELETE: api/Menus/5
+        [ResponseType(typeof(Menu))]
+        public IHttpActionResult DeleteMenu(long id)
+        {
+            Menu menu = db.Menus.Find(id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            db.Menus.Remove(menu);
+            db.SaveChanges();
+
+            return Ok(menu);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MenuExists(long id)
+        {
+            return db.Menus.Count(e => e.ID == id) > 0;
+        }
+    }
+}

7dd7000dc688da6d12198639265316f9a60824d1
kanangandhi93 <kanangandhi93@gmail.com>
2018-05-19 15:34:47 +0530

    Web Design Completed.

---
+++ Web/Controllers/DirItemMaster/ItemGroupController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers.DirItemMaster
+{
+    public class ItemGroupController : Controller
+    {
+        // GET: ItemGroup
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

bb1f3c128e89d75c0b08fcf54a9fc5a09854cac4
kanangandhi93 <kanangandhi93@gmail.com>
2018-05-19 10:47:04 +0530

    Payment Completed/

---
+++ api/Controllers/apiPaymentDir/PaymentReceiptDetailsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using api.Models;
+
+namespace api.Controllers.apiPaymentDir
+{
+    public class PaymentReceiptDetailsController : ApiController
+    {
+        private InventoryControlEntities db = new InventoryControlEntities();
+
+        // GET: api/PaymentReceiptDetails
+        public IEnumerable<PaymentReceiptDetail> GetPaymentReceiptDetails()
+        {
+            return db.PaymentReceiptDetails;
+        }
+
+        // GET: api/PaymentReceiptDetails/5
+        [ResponseType(typeof(PaymentReceiptDetail))]
+        public IHttpActionResult GetPaymentReceiptDetail(long id)
+        {
+            PaymentReceiptDetail paymentReceiptDetail = db.PaymentReceiptDetails.Find(id);
+            if (paymentReceiptDetail == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(paymentReceiptDetail);
+        }
+
+        // PUT: api/PaymentReceiptDetails/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPaymentReceiptDetail(long id, PaymentReceiptDetail paymentReceiptDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != paymentReceiptDetail.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(paymentReceiptDetail).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentReceiptDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/PaymentReceiptDetails
+        [ResponseType(typeof(PaymentReceiptDetail))]
+        public IHttpActionResult PostPaymentReceiptDetail(PaymentReceiptDetail paymentReceiptDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.PaymentReceiptDetails.Add(paymentReceiptDetail);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = paymentReceiptDetail.ID }, paymentReceiptDetail);
+        }
+
+        // DELETE: api/PaymentReceiptDetails/5
+        [ResponseType(typeof(PaymentReceiptDetail))]
+        public IHttpActionResult DeletePaymentReceiptDetail(long id)
+        {
+            PaymentReceiptDetail paymentReceiptDetail = db.PaymentReceiptDetails.Find(id);
+            if (paymentReceiptDetail == null)
+            {
+                return NotFound();
+            }
+
+            db.PaymentReceiptDetails.Remove(paymentReceiptDetail);
+            db.SaveChanges();
+
+            return Ok(paymentReceiptDetail);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PaymentReceiptDetailExists(long id)
+        {
+            return db.PaymentReceiptDetails.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ api/Controllers/apiPaymentDir/PaymentReceiptMastersController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using api.Models;
+
+namespace api.Controllers.apiPaymentDir
+{
+    public class PaymentReceiptMastersController : ApiController
+    {
+        private InventoryControlEntities db = new InventoryControlEntities();
+
+        // GET: api/PaymentReceiptMasters
+        public IEnumerable<PaymentReceiptMaster> GetPaymentReceiptMasters()
+        {
+            return db.PaymentReceiptMasters;
+        }
+
+        // GET: api/PaymentReceiptMasters/5
+        [ResponseType(typeof(PaymentReceiptMaster))]
+        public IHttpActionResult GetPaymentReceiptMaster(long id)
+        {
+            PaymentReceiptMaster paymentReceiptMaster = db.PaymentReceiptMasters.Find(id);
+            if (paymentReceiptMaster == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(paymentReceiptMaster);
+        }
+
+        // PUT: api/PaymentReceiptMasters/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPaymentReceiptMaster(long id, PaymentReceiptMaster paymentReceiptMaster)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != paymentReceiptMaster.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(paymentReceiptMaster).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentReceiptMasterExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/PaymentReceiptMasters
+        [ResponseType(typeof(PaymentReceiptMaster))]
+        public IHttpActionResult PostPaymentReceiptMaster(PaymentReceiptMaster paymentReceiptMaster)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.PaymentReceiptMasters.Add(paymentReceiptMaster);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (PaymentReceiptMasterExists(paymentReceiptMaster.ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = paymentReceiptMaster.ID }, paymentReceiptMaster);
+        }
+
+        // DELETE: api/PaymentReceiptMasters/5
+        [ResponseType(typeof(PaymentReceiptMaster))]
+        public IHttpActionResult DeletePaymentReceiptMaster(long id)
+        {
+            PaymentReceiptMaster paymentReceiptMaster = db.PaymentReceiptMasters.Find(id);
+            if (paymentReceiptMaster == null)
+            {
+                return NotFound();
+            }
+
+            db.PaymentReceiptMasters.Remove(paymentReceiptMaster);
+            db.SaveChanges();
+
+            return Ok(paymentReceiptMaster);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PaymentReceiptMasterExists(long id)
+        {
+            return db.PaymentReceiptMasters.Count(e => e.ID == id) > 0;
+        }
+    }
+}

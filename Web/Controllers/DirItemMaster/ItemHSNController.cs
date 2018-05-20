62c0c692e4e1d0a0e5007b0520a5df3d282f7642
kanangandhi93 <kanangandhi93@gmail.com>
2018-05-20 10:28:07 +0530

    Menu Partial COmpleted.

---
+++ Web/Controllers/DirItemMaster/ItemHSNController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers.DirItemMaster
+{
+    public class ItemHSNController : Controller
+    {
+        // GET: ItemHSN
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web/Controllers/MenuMasterDir/menuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers.MenuMasterDir
+{
+    public class menuController : Controller
+    {
+        // GET: menu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

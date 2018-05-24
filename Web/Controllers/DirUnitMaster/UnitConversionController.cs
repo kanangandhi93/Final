7060deb0d3e316fc227aa839bab76180142b546e
kanangandhi93 <kanangandhi93@gmail.com>
2018-05-24 22:25:16 +0530

    Unit Conversion Design Complete.

---
+++ Web/Controllers/DirUnitMaster/UnitConversionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers.DirUnitMaster
+{
+    public class UnitConversionController : Controller
+    {
+        // GET: UnitConversion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
